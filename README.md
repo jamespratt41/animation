2018c3a955477d7583499fca1fca7b5e05d4c722
James Pratt <james.r.pratt41@gmail.com>
2019-01-28 12:42:18 -0500

    added files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# animation
+# My image animation.
+#If you're here you must know the basics of a computer.
+# Your mother must be so proud.
--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,4 +1,21 @@
 (() => {
+
+	console.log('boomtown');
+
 	// set up the puzzle pieces and boards
-	
+
+	const theButton = document.querySelector("#buttonHolder img");
+
+
+	function changeHeadLine(){
+		document.querySelector('h1').textContent = "YOO WASSUP!";
+		document.querySelector('p').textContent = "Party time";
+	}
+
+	theButton.addEventListener('click',changeHeadLine);
+
+
+
+
+
 })();
